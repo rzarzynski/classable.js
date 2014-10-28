c3959b47716901d8cc29c57f47ffda12def2e999
Radoslaw Zarzynski <radoslaw@zarzynski.pl>
2014-10-28 13:00:01 +0100

    Add support for UMD (especially RequireJS).

--- extobj.js
+++ extobj.js
@@ -15,30 +15,42 @@
  * License: GNU GPL v2
  */
 
-'use strict'
+'use strict';
 
-var ExtObj = new (function () {
-    this.prototype = {};
-    this.extend = function () {
-        var that = this;
-
-        return new (function () {
-            function newClass () {
-                if (newClass.prototype.hasOwnProperty('initialize')
-                        && typeof this.initialize == 'function') {
-                    this.initialize(arguments);
-                }
-            }
-
-            newClass.extend    = that.extend;
-            newClass.prototype = Object.create(that.prototype);
-
-            /*
-             * Define the "super" handle to access parent.
-             * TODO: use defineProperty() to make it nonenumerable.
-             */
-            newClass.prototype._super = that.prototype;
-            return newClass;
-        })();
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        /* AMD. Register as an anonymous module. */
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        /* Node. Does not work with strict CommonJS, but
+         * only CommonJS-like environments that support module.exports,
+         * like Node. */
+        module.exports = factory();
+    } else {
+        /* Browser globals (root is window). */
+        root.ExtObj = factory();
     }
-})();
+}(this, function () {
+    return new (function () {
+        this.prototype = {};
+        this.extend = function () {
+            var that = this;
+
+            return new (function () {
+                function newClass () {
+                    if (newClass.prototype.hasOwnProperty('initialize')
+                            && typeof this.initialize === 'function') {
+                        this.initialize(arguments);
+                    }
+                }
+                newClass.extend    = that.extend;
+                newClass.prototype = Object.create(that.prototype);
+
+                /* Define the "super" handle to access parent.
+                 * TODO: use defineProperty() to make it nonenumerable. */
+                newClass.prototype._super = that.prototype;
+                return newClass;
+            })();
+        }
+    })();
+}));
